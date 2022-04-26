f88572eeb20b18a24656201ff157bd8dcbaea525
Jocelyn Tandrea <jta189@sfu.ca>
2022-04-25 17:56:33 -0700

    Add ImageOperations class to decode and encode image bitmaps

---
+++ app/src/main/java/com/example/foodapp/serializer/ImageOperations.java
@@ -0,0 +1,24 @@
+package com.example.foodapp.serializer;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageOperations {
+    //Encodes bitmap into a String
+    public static String encodeBitmap(Bitmap image) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        byte[] bytes = outputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    //Decodes String into bitmap
+    public static Bitmap decodeBitmap(String encodedString) {
+        byte[] decodedByte = Base64.decode(encodedString, 0);
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+    }
+
+}
